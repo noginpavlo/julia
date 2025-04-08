84f7b46d9194bed3d69963b3b46d25840c44a448
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-08 16:12:56 +0200

    mande views.py update the db

--- card_manager/views.py
+++ card_manager/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from card_manager.services.card_dealer import save_data
 from django.http import HttpResponse
+
 
 def save_card_view(request):
     array = ["2025-04-07", "hello", "həˈləʊ", "A greeting", "Hello, world!", 1]
--- julia/urls.py
+++ julia/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from card_manager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("home.urls"))
+    path('', include("home.urls")),
+    path('save-card/', views.save_card_view, name='save_card'),
 ]
