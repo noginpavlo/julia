96292f8c1ce2b9b5e4a4c4666da0fe3e348c45e0
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-05 12:26:29 +0200

    Initial commit before making code ready to new fearure implementation (search bar)

--- card_manager/api/serializers.py
+++ card_manager/api/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from card_manager.models import Deck, Card
 
+
 class DeckSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deck
         fields = ['id', 'deck_name', 'date_created', 'date_updated']
+
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,3 +21,15 @@ class CardSerializer(serializers.ModelSerializer):
             'repetitions',
             'interval'
         ]
+
+
+# class CardNameOnlySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Card
+#         fields = ['id', 'word']
+#
+#
+# class DeckNameOnlySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Deck
+#         fields = ['id', 'deck_name', 'date_created', 'date_updated']
