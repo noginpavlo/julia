3a89d8043aff1160efd8cd930ce0e91a55938775
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-27 14:44:33 +0200

    Registered card_manager models in admin.py to view in django-admin

--- card_manager/admin.py
+++ card_manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Deck, Card, ShowCardDailyStat
 
-# Register your models here.
+admin.site.register(Deck)
+admin.site.register(Card)
+admin.site.register(ShowCardDailyStat)
