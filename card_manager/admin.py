b97441cd1425ed33ff684bf167c8bb3c90ac42bd
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-10-25 11:36:26 +0200

    Changed the way I refine the column names with @admin.dispay decorator instead of the function

--- card_manager/admin.py
+++ card_manager/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
-from django.copyright.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 
 from .models import Card, Deck, ShowCardDailyStat
 
@@ -12,12 +12,11 @@ class CardAdmin(ModelAdmin):
 
     list_display = ("word", "deck", "get_user")
 
+    @admin.display(description="user", ordering="deck_user")
     def get_user(self, obj: Card) -> AbstractBaseUser:
         """Gets the user from Deck model as it is fk to Deck."""
 
         return obj.deck.user
-
-    get_user.short_description = "user"  # sets column name as USER in admin
 
 
 admin.site.register(Card, CardAdmin)
