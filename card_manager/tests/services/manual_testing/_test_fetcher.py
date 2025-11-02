0feec32c4638647c3c88ba3ea0622b1669b48b02
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-11-02 10:04:55 +0100

    Tested validator from client perspective

--- card_manager/tests/services/manual_testing/_test_fetcher.py
+++ card_manager/tests/services/manual_testing/_test_fetcher.py
@@ -2,6 +2,8 @@
 Manual testing script (client mocking) for fetcher module.
 """
 
+import json
+
 from card_manager.services.fetcher import (
     DICTIONARYAPI_URL,
     DictApiErrorMapper,
@@ -9,13 +11,16 @@ from card_manager.services.fetcher import (
     WordFetcherService,
 )
 from card_manager.services.providers import RequestsProvider
+from card_manager.services.validator import DictApiValidator
 
 requests_provider = RequestsProvider()
 fetcher = DictApiFetcher(requests_provider, DICTIONARYAPI_URL)
 mapper = DictApiErrorMapper()
 fetcher_service = WordFetcherService(fetcher, mapper)
 
-result = fetcher_service.get_fetched_word("cat")
+response = fetcher_service.get_fetched_word("cat")
 
+print(json.dumps(response, indent=4))
 
-print(result)
+validator = DictApiValidator(response)
+print(validator.validate_response())
